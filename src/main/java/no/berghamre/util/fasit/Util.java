17ee6455ef6f6529d6d734bc8f35e2977cf49dfa
Kristian Berg <rasmantuta@gmail.com>
2014-03-19 23:45:54 +0100

    Fixed FibonacciGeneratorTest

--- src/main/java/no/berghamre/util/fasit/Util.java
+++ src/main/java/no/berghamre/util/fasit/Util.java
@@ -3,7 +3,10 @@ package no.berghamre.util.fasit;
 import no.berghamre.data.Gender;
 import no.berghamre.data.IncomeStatistics;
 
+import java.util.Random;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * This is a utility class for the provide "solution" of the exercises. Many of theese methods would
@@ -30,4 +33,9 @@ public class Util {
   public static int compareByIncome(IncomeStatistics is1, IncomeStatistics is2){
     return is1.averageIncome - is2.averageIncome;
   }
+
+//    Stream s = Stream.builder().add(1).add(2).add(3).build();
+//    IntStream positiveBelow100 = new Random(7).ints(1, 100);
+//    Stream s2 = Stream.iterate(3, s -> s+3);
+
 }
--- src/test/java/no/berghamre/util/FibonacciGeneratorTest.java
+++ src/test/java/no/berghamre/util/FibonacciGeneratorTest.java
@@ -1,5 +1,6 @@
 package no.berghamre.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Random;
@@ -8,21 +9,12 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import no.berghamre.util.fasit.FibonacciGenerator;
 
 public class FibonacciGeneratorTest {
     @Test
     public void testInfiniteStream() throws Exception {
-        FibonacciGenerator.fibonacci().limit(10).forEach(System.out::println);//filter(l -> l<1000).forEach(System.out::println);
+        long[] fibs = FibonacciGenerator.fibonacci().limit(10).toArray();
+        Assert.assertArrayEquals(new long[]{1, 1, 2, 3, 5, 8, 13, 21, 34, 55}, fibs);
     }
 
-    Stream s = Stream.builder().add(1).add(2).add(3).build();
-    IntStream positiveBelow100 = new Random(7).ints(1, 100);
-    Stream s2 = Stream.iterate(3, s -> s+3);
-
-    @Test
-    public void testIterate() throws Exception {
-        System.out.println(s2.limit(10).collect(Collectors.toList()));
-
-    }
 }
